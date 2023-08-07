bb6ab690555c7b8c7e1033fe15aba704d5c69727
xuzhangtao <xuzhangtao@csii.com>
2023-08-07 09:38:24 +0800

    1. 配置修改支持spine和sound

--- game/src/index.js
+++ game/src/index.js
@@ -6,6 +6,7 @@ import 'whatwg-fetch';
 // Apply the patch to PIXI
 install({ ShaderSystem });
 import * as PIXI from 'pixi.js'
+import {Spine} from 'pixi-spine'
 
 // 获取适配屏幕大小
 const {pixelRatio, windowWidth, windowHeight} = wx.getSystemInfoSync();
@@ -17,71 +18,43 @@ let app = new PIXI.Application({
 });
 document.body.appendChild(app.view);
 
-app.stop();
+PIXI.Assets.load('assets/pixi-spine/dragon.json').then(onAssetsLoaded);
 
-// load resources
-PIXI.Assets.load('assets/spritesheet/monsters.json')
-    .then(onAssetsLoaded);
+function onAssetsLoaded(dragonAsset) {
+    // instantiate the spine animation
+    const dragon = new Spine(dragonAsset.spineData);
+    dragon.skeleton.setToSetupPose();
+    dragon.update(0);
+    dragon.autoUpdate = false;
 
-// holder to store aliens
-const aliens = [];
-const alienFrames = [
-    'eggHead.png',
-    'flowerTop.png',
-    'helmlok.png',
-    'skully.png',
-];
+    // create a container for the spine animation and add the animation to it
+    const dragonCage = new PIXI.Container();
+    dragonCage.addChild(dragon);
 
-let count = 0;
+    // measure the spine animation and position it inside its container to align it to the origin
+    const localRect = dragon.getLocalBounds();
+    dragon.position.set(-localRect.x, -localRect.y);
 
-// create an empty container
-const alienContainer = new PIXI.Container();
-alienContainer.x = 400;
-alienContainer.y = 300;
+    // now we can scale, position and rotate the container as any other display object
+    const scale = Math.min(
+        (app.screen.width * 0.7) / dragonCage.width,
+        (app.screen.height * 0.7) / dragonCage.height,
+    );
+    dragonCage.scale.set(scale, scale);
+    dragonCage.position.set(
+        (app.screen.width - dragonCage.width) * 0.5,
+        (app.screen.height - dragonCage.height) * 0.5,
+    );
 
-// make the stage interactive
-app.stage.interactive = true;
-app.stage.addChild(alienContainer);
+    // add the container to the stage
+    app.stage.addChild(dragonCage);
 
-function onAssetsLoaded() {
-    // add a bunch of aliens with textures from image paths
-    for (let i = 0; i < 100; i++) {
-        const frameName = alienFrames[i % 4];
+    // once position and scaled, set the animation to play
+    dragon.state.setAnimation(0, 'flying', true);
 
-        // create an alien using the frame name..
-        const alien = PIXI.Sprite.from(frameName);
-        alien.tint = Math.random() * 0xFFFFFF;
 
-        alien.x = Math.random() * 800 - 400;
-        alien.y = Math.random() * 600 - 300;
-        alien.anchor.x = 0.5;
-        alien.anchor.y = 0.5;
-        aliens.push(alien);
-        alienContainer.addChild(alien);
-    }
-    app.start();
+    app.ticker.add(() => {
+        // update the spine animation, only needed if dragon.autoupdate is set to false
+        dragon.update(app.ticker.deltaMS / 1000); // IN SECONDS!
+    });
 }
-
-// Combines both mouse click + touch tap
-app.stage.on('pointertap', onClick);
-
-function onClick() {
-    alienContainer.cacheAsBitmap = !alienContainer.cacheAsBitmap;
-}
-
-app.ticker.add(() => {
-    // let's rotate the aliens a little bit
-    for (let i = 0; i < 100; i++) {
-        const alien = aliens[i];
-        alien.rotation += 0.1;
-    }
-
-    count += 0.01;
-
-    alienContainer.scale.x = Math.sin(count);
-    alienContainer.scale.y = Math.sin(count);
-    alienContainer.rotation += 0.01;
-});
-
-
-
